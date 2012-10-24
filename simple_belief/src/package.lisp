e42a7601864f4906c28b4e3da1956c7a1653af57
Jan Winkler <winkler@cs.uni-bremen.de>
2012-10-24 07:54:29 +0200

    Cleaned up the package.lisp file in simple_belief

--- simple_belief/src/package.lisp
+++ simple_belief/src/package.lisp
@@ -35,6 +35,15 @@
    #:cram-reasoning
    #:cram-process-modules
    #:cram-plan-library #:cram-plan-knowledge #:cram-utilities)
-  (:shadowing-import-from #:cram-plan-knowledge event object-attached object-in-hand event-side object-detached event-object on-event ?side holds)
-  (:desig-properties #:pose #:gripper #:in #:left #:right #:radius #:shape #:collision-part
-                     #:cylinder #:length))
+  (:shadowing-import-from #:cram-plan-knowledge
+                          event
+                          object-attached
+                          object-in-hand
+                          event-side
+                          object-detached
+                          event-object
+                          on-event
+                          ?side
+                          holds)
+  (:desig-properties #:pose #:gripper #:in #:left #:right #:radius
+                     #:shape #:collision-part #:cylinder #:length))
